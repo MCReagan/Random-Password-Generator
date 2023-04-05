a7c200a343303c8f08d750655e7e03eddc5de27a
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-04-05 15:27:28 -0500

    added prompts to user to determine password string value

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,31 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+    var password = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !#$%&'()*+,-./:;<=>?@[]^\_`{|}~";
+
+    if (confirm("Would you like to include Upper Case characters?")) {
+        var passwordReplace = password;
+    } else {
+        var passwordReplace = password.replace("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "");
+    };
+
+    if (confirm("Would you like to include lower case characters")) {
+        var passwordReplace1 = passwordReplace;
+    } else {
+        var passwordReplace1 = passwordReplace.replace("abcdefghijklmnopqrstuvwxyz", "");
+    };
+
+    if (confirm("Would you like to include numbers?")) {
+        var passwordReplace2 = passwordReplace1;
+    } else {
+        var passwordReplace2 = passwordReplace1.replace("0123456789", "");
+    };
+
+    if (confirm("Would you like to include special characters?")) {
+        var passwordReplace3 = passwordReplace2;
+    } else {
+        var passwordReplace3 = passwordReplace2.replace(" !#$%&'()*+,-./:;<=>?@[]^\_`{|}~", "");
+    };
 };
 
 function writePassword() {
